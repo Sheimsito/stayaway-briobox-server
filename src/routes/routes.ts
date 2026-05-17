79c52ca5cb273dd2e42370c3acb6b0776037b999
Kewin <kealgri@gmail.com>
2026-05-17 15:05:43 -0500

    fix: Forgot to put membership routes on routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRoutes from "./userRoutes.js";
 import { authenticateToken } from "../middleware/auth.js";
 import authRoutes from "./authRoutes.js";
+import membershipRoutes from "./membershipRoutes.js";
 
 const router = Router();
 
@@ -12,5 +13,7 @@ router.use("/auth", authRoutes);
 // Then implement the protected routes here. ( by Auth.js )
 
 router.use("/users",authenticateToken , userRoutes);
+
+router.use("/memberships",authenticateToken , membershipRoutes);
 
 export default router;
