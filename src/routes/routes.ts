ec84130a227b60c4294357928884ea052628f97d
Kewin <kealgri@gmail.com>
2026-05-22 14:33:48 -0500

    fix: added products and suppliers routes on routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,6 +5,8 @@ import authRoutes from "./authRoutes.js";
 import membershipRoutes from "./membershipRoutes.js";
 import paymentRoutes from './paymentRoutes.js';
 import cashRegisterRoutes from './cashRegisterRoutes.js';
+import productRoutes from "./productRoutes.js";
+import supplierRoutes from "./supplierRoutes.js";
 
 const router = Router();
 
@@ -21,5 +23,8 @@ router.use("/memberships",authenticateToken , membershipRoutes);
 router.use('/payments', authenticateToken, paymentRoutes);
 
 router.use('/cash-register', cashRegisterRoutes);
+
+router.use('/products', authenticateToken, productRoutes);
+router.use('/suppliers', authenticateToken, supplierRoutes);
 
 export default router;
